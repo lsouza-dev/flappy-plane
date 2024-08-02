856b16a48ab9aae0ba58cae4400cae303216d126
Luiz Fabiano de Souza <LuizFabianoDS04@gmail.com>
2024-08-02 17:29:56 -0300

    Atualização sexta-feira

--- Scripts/SpawnManager.cs
+++ Scripts/SpawnManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Security;
 using System.Security.Principal;
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
 
@@ -13,11 +14,17 @@ public class SpawnManager : MonoBehaviour
     [SerializeField] private float timerToSpawn = 1f;
     [SerializeField] private float timeToDestroy;
 
+    [SerializeField] private float timeToSpawnMin = 1f;
+    [SerializeField] private float timeToSpawnMax = 3f;
+
     [SerializeField] private float yMin;
     [SerializeField] private float yMax;
 
     [SerializeField] private Vector3 pos;
 
+    [SerializeField] private float points = 1f;
+    [SerializeField] private int aliveSeconds;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -27,17 +34,31 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        timer -= Time.deltaTime;
+        SpawnObstacles();
+        CountPoints();
+    }
 
+    private void SpawnObstacles()
+    {
+        timer -= Time.deltaTime;
         Vector2 randomPos = new Vector2(pos.x, Random.Range(yMin, yMax));
 
         if (timer <= 0f)
         {
             GameObject obstacleInstance = Instantiate(obstaclePrefab, randomPos, Quaternion.identity);
-            Destroy(obstacleInstance,timeToDestroy);
-            timerToSpawn = Random.Range(2, 4);
+            Destroy(obstacleInstance, timeToDestroy);
+            timerToSpawn = Random.Range(timeToSpawnMin, timeToSpawnMax);
             timer = timerToSpawn;
 
         }
     }
+
+    private void CountPoints()
+    {
+        points += Time.deltaTime;
+
+    }
+    
 }
+
+
